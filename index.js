2de3603fc109aa5f6dd925ab4a905dc3e413d217
Tom George <utduu@student.kit.edu>
2018-01-22 17:57:14 +0100

    ..

--- index.js
+++ index.js
@@ -48,16 +48,16 @@ function redraw_canvas(event){
     var c = document.getElementById("ed_canvas");
     var image = new Image();
     image.src = c.toDataURL();
-    debug.innerHTML = "image width: "+image.width+", image height: "+image.height;
+    debug.innerHTML = "image width: "+image.clientWidth+", image height: "+image.clientHeight;
     c.width = window.innerWidth*0.95;
     c.height = window.innerHeight*0.7;
     c.style.width = c.width;
     c.style.height = c.height;
     
     if(document.getElementById("editor").style.display=="inline"){
-	var ratio = image.width/image.height;
+	var ratio = image.clientWidth/image.clientHeight;
 	debug.innerHTML=debug.innerHTML+", ratio: "+ratio;
-	if(image.width>c.width){
+	if(image.clientWidth>c.width){
 	    c.getContext('2d').drawImage(image,0,0, c.width, c.width/ratio);
 	}
 	else{
