951e22e7991c4aeb3a7f8864d038202ab0aaca32
Tom George <utduu@student.kit.edu>
2018-01-22 16:04:53 +0100

    debugging for ipad

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function redraw_canvas(event){
     c.style.width = c.width;
     c.style.height = c.height;
     
-    if(document.getElementById("editor").style.display!="none"){
+    if(document.getElementById("editor").style.display=="inline"){
 	documents.getElementsByTagName("h1")[0].innerHTML = "image detected";
 	var image = new Image();
 	image.src = c.toDataURL();
